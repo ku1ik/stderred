32efac4e945686f5aa0079e03956a0246aafa26a
Asheesh Laroia <asheesh@asheesh.org>
2008-09-27 20:44:55 -0700

    colors, so far (but not for ls)

--- hack_write.c
+++ hack_write.c
@@ -5,17 +5,39 @@
 
 #undef fwrite
 
-int fwrite(const void *ptr, int size, int nmemb,
-	FILE *stream) {
+/* SOURCE: http://deathray.us/code/color_output.html */
 
+static const char COL_RESET[] = "\x1b[0m";
+
+/*  Foreground colors are in form of 3x, bacground are 4x */
+static const char RED[]     = "\x1b[31m";
+static const char GREEN[]   = "\x1b[32m";
+static const char YELLOW[]  = "\x1b[33m";
+static const char BLUE[]    = "\x1b[34m";
+static const char MAGENTA[] = "\x1b[35m";
+static const char CYAN[]    = "\x1b[36m";
+
+/* Not including background colors for no good reason */
+
+int fwrite(const void *ptr, int size, int nmemb,
+	   FILE *stream) {
+  
   void * libc = dlopen("/lib/libc.so.6", RTLD_LAZY); /* never closed, rofl */
   int (*lol_fwrite) (const void *, int, int, FILE *);
   *(void **) (&lol_fwrite) = dlsym(libc, "fwrite");
+  
+  if (stream  == stderr) {
+    (*lol_fwrite)(RED, 1, sizeof(RED), stream);
+  } else if (stream == stdout) {
+    (*lol_fwrite)(BLUE, 1, sizeof(BLUE), stream);
+  }
+
+  /* always actually do the write() */
   (*lol_fwrite)(ptr, size, nmemb, stream);
-
-	
-	/* (*lol_fwrite)("rofl\n", 1, 5, stream);
-	(*lol_fwrite)(ptr, size, nmemb, stream);
-	(*lol_fwrite)("unrofl\n", 1, 5, stream); */
+  
+  if ( (stream == stderr) ||
+       (stream == stdout )) {
+    (*lol_fwrite)(COL_RESET, 1, sizeof(COL_RESET), stream);
+  }
+  
 }
-
