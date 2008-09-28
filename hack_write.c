d0144aaa62d7ecc0de59c126f5568e92b0551de4
Asheesh Laroia <asheesh@asheesh.org>
2008-09-27 20:47:15 -0700

    +fwrite_unlocked

--- hack_write.c
+++ hack_write.c
@@ -1,9 +1,11 @@
 #define fwrite ye_olde_fwrite
+#define fwrite_unlocked ye_olde_fwrite_unlocked
 #include <stdio.h>
 
 #include <dlfcn.h>
 
 #undef fwrite
+#undef fwrite_unlocked
 
 /* SOURCE: http://deathray.us/code/color_output.html */
 
@@ -41,3 +43,28 @@ int fwrite(const void *ptr, int size, int nmemb,
   }
   
 }
+
+
+int fwrite_unlocked(const void *ptr, int size, int nmemb,
+	   FILE *stream) {
+  
+  void * libc = dlopen("/lib/libc.so.6", RTLD_LAZY); /* never closed, rofl */
+  int (*lol_fwrite_unlocked) (const void *, int, int, FILE *);
+  *(void **) (&lol_fwrite_unlocked) = dlsym(libc, "fwrite_unlocked");
+  
+  if (stream  == stderr) {
+    (*lol_fwrite_unlocked)(RED, 1, sizeof(RED), stream);
+  } else if (stream == stdout) {
+    (*lol_fwrite_unlocked)(BLUE, 1, sizeof(BLUE), stream);
+  }
+
+  /* always actually do the write() */
+  (*lol_fwrite_unlocked)(ptr, size, nmemb, stream);
+  
+  if ( (stream == stderr) ||
+       (stream == stdout )) {
+    (*lol_fwrite_unlocked)(COL_RESET, 1, sizeof(COL_RESET), stream);
+  }
+  
+}
+
