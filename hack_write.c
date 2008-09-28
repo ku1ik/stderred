2d18a209e609ef72418d4e113ceee023c208e7a6
Asheesh Laroia <asheesh@asheesh.org>
2008-09-27 22:27:46 -0700

    great success

--- hack_write.c
+++ hack_write.c
@@ -1,5 +1,7 @@
 #define write ye_olde_write
 #include <unistd.h>
+#include <string.h>
+#include <alloca.h>
 
 #include <dlfcn.h>
 
@@ -20,27 +22,21 @@ static const char CYAN[]    = "\x1b[36m";
 /* Not including background colors for no good reason */
 
 int write(int fd, const void* buf, int count) {
-  int destination_fd = fd;
+  /* Always: yoink old write from libc */
   void * libc = dlopen("/lib/libc.so.6", RTLD_LAZY); /* never closed, rofl */
   int (*lol_write) (int, const void *, int);
-  *(void **) (&lol_write) = dlsym(libc, "write");
+  *(void **) (&lol_write) = dlsym(libc, "write"); 
 
-  /* always do the write, but if it was to either 1 or 2 put it on 1 */
-  if ( (fd == 1) ||
-       (fd == 2)) {
-    destination_fd = 1;
-  }
-
-  if (fd == 2) /* stderr */ {
-    (*lol_write)(destination_fd, RED, sizeof(RED));
-  }
-
-  (*lol_write)(destination_fd, buf, count);
-  
   if (fd == 2) {
-    (*lol_write)(destination_fd, COL_RESET, sizeof(COL_RESET));
-  }  
-  
+    /* Do crazy nonsense to buf and count */
+    int new_count = count + sizeof(RED) + sizeof(COL_RESET);
+    void * new_buf = alloca(new_count);
+    memcpy(new_buf, RED, sizeof(RED));
+    memcpy(new_buf + sizeof(RED), buf, count);
+    memcpy(new_buf + sizeof(RED) + count, COL_RESET, sizeof(COL_RESET));
+    (*lol_write)(fd, new_buf, new_count);
+  }
+  else {
+    (*lol_write)(fd, buf, count);
+  }
 }
-
-  
