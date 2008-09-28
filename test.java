0c8d5e422db5c8886f58d1b90508c1ab22d2cbaf
Asheesh Laroia <asheesh@asheesh.org>
2008-09-28 00:12:19 -0700

    Oops, forgot to add the demo Java program

---
+++ test.java
@@ -0,0 +1,9 @@
+class test
+{  
+        public static void main(String args[])
+        {
+           System.out.println("Hello World!");
+           System.err.println("Hello error World!");
+        }
+}
+
