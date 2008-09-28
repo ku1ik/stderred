acbcb8c8fb19c9da5983f0b44cefdbf391fec081
Asheesh Laroia <asheesh@asheesh.org>
2008-09-27 20:35:38 -0700

    get old fwrite via dlopen()

--- hack_write.c
+++ hack_write.c
@@ -1,7 +1,21 @@
+#define fwrite ye_olde_fwrite
+#include <stdio.h>
 
+#include <dlfcn.h>
 
-ssize_t fwrite(const void *ptr, size_t size, size_t nmemb,
+#undef fwrite
+
+int fwrite(const void *ptr, int size, int nmemb,
 	FILE *stream) {
-	write(1, "rofl");
+
+  void * libc = dlopen("/lib/libc.so.6", RTLD_LAZY); /* never closed, rofl */
+  int (*lol_fwrite) (const void *, int, int, FILE *);
+  *(void **) (&lol_fwrite) = dlsym(libc, "fwrite");
+  (*lol_fwrite)(ptr, size, nmemb, stream);
+
+	
+	/* (*lol_fwrite)("rofl\n", 1, 5, stream);
+	(*lol_fwrite)(ptr, size, nmemb, stream);
+	(*lol_fwrite)("unrofl\n", 1, 5, stream); */
 }
 
