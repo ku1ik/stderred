d8586642f43a066afd8e73e677f699d832a50bcc
Marcin Kulik <marcin.kulik@gmail.com>
2011-12-13 21:31:09 +0100

    Remove java test class

--- test.java
+++
@@ -1,9 +0,0 @@
-class test
-{  
-        public static void main(String args[])
-        {
-           System.out.println("Hello World!");
-           System.err.println("Hello error World!");
-        }
-}
-
