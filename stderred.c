5a0c66455c098344f6e406616cad1a33760b773b
Chris Hoffman <cehoffman@gmail.com>
2012-04-30 00:10:11 -0500

    Overwrite fputc, fputs, fwrite, fprintf, vfprintf, perror

--- stderred.c
+++ stderred.c
@@ -1,76 +1,147 @@
+#include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
 #include <string.h>
-#include <sys/uio.h>
-
-#undef write
+#include <errno.h>
+#include <stdarg.h>
 
 static const char RED[]       = "\x1b[31m";
 static const char COL_RESET[] = "\x1b[0m";
 #define COL_RESET_SIZE sizeof(COL_RESET)-1
 
-// This macro was taken from
-// http://www.mikeash.com/pyblog/friday-qa-2009-01-30-code-injection.html#comment-3fb6e4b8cf65ec984e7836e2b86a2875
-#define DYLD_INTERPOSE(_replacment,_replacee) \
-__attribute__((used)) static struct{ const void* replacment; const void* replacee; } _interpose_##_replacee \
-__attribute__ ((section ("__DATA,__interpose"))) = { (const void*)(unsigned long)&_replacment, (const void*)(unsigned long)&_replacee };
-
 #ifdef __APPLE__
-  DYLD_INTERPOSE(write, write);
+  // This macro was taken from
+  // http://www.mikeash.com/pyblog/friday-qa-2009-01-30-code-injection.html#comment-3fb6e4b8cf65ec984e7836e2b86a2875
+  #define DYLD_INTERPOSE(_replacment,_replacee) \
+    __attribute__((used)) static struct{ const void* replacment; const void* replacee; } _interpose_##_replacee \
+    __attribute__ ((section ("__DATA,__interpose"))) = { (const void*)(unsigned long)&_replacment, (const void*)(unsigned long)&_replacee };
+  #define FUNC(name) _##name
+  #define ORIGINAL(name) name
+  #define GET_ORIGINAL(...)
+#else
+  #include <dlfcn.h>
+  #define ORIGINAL(name) original_##name
+  #define GET_ORIGINAL(ret, name, ...) \
+    static ret (*ORIGINAL(name))(__VA_ARGS__) = NULL; \
+    if (!ORIGINAL(name)) { \
+      *(void **)(&ORIGINAL(name)) = dlsym(RTLD_NEXT, #name); \
+    } \
+    if (!ORIGINAL(name)) { \
+      errno = ENOSYS; \
+      return -1; \
+    }
+  #define FUNC(name) name
+#endif
+
+#ifdef __BSD__
+  #define PROGNAME getprogname()
+#else
+  extern char *program_invocation_name;
+  #define PROGNAME program_invocation_name
 #endif
 
 char *color_code;
 int color_code_size;
 
-#define _reset() { \
-  struct iovec vec = { (char *)COL_RESET, COL_RESET_SIZE }; \
-  writev(STDERR_FILENO, &vec, 1); \
+#define RESET() ORIGINAL(write)(STDERR_FILENO, COL_RESET, COL_RESET_SIZE)
+
+#define GET_COLOR_CODE() { \
+  if (color_code == NULL) { \
+    color_code = getenv("STDERRED_ESC_CODE"); \
+    if (color_code == NULL) { \
+      color_code = (char *)RED; \
+    } \
+    color_code_size = strlen(color_code); \
+  } \
 }
 
-ssize_t write(int fd, const void* buf, size_t count) {
-  if (color_code == NULL) {
-    color_code = getenv("STDERRED_ESC_CODE");
+ssize_t FUNC(write)(int fd, const void* buf, size_t count) {
+  GET_ORIGINAL(ssize_t, write, int, const void *, size_t);
 
-    if (color_code == NULL) {
-      color_code = (char *)RED;
-    }
-
-    color_code_size = strlen(color_code);
+  if (fd != STDERR_FILENO || !isatty(STDERR_FILENO)) {
+    ORIGINAL(write)(fd, buf, count);
   }
 
-  if (fd == STDERR_FILENO && isatty(STDERR_FILENO)) {
-    struct iovec vec[3] = {
-      { (char *)color_code, color_code_size },
-      { (char *)buf, count },
-      { (char *)COL_RESET, COL_RESET_SIZE }
-    };
+  GET_COLOR_CODE();
 
-    ssize_t written = writev(fd, vec, sizeof(vec) / sizeof(vec[0]));
+  ssize_t written = ORIGINAL(write)(fd, color_code, color_code_size);
+  if (written <= 0) return written;
 
-    if (written <= 0)
-      return written;
-
-    if (written <= color_code_size) {
-      _reset();
-      return 0;
-    }
-
-    written -= color_code_size;
-
-    if (written <= count) {
-      _reset();
-      return written;
-    }
-
-    written -= count;
-
-    if (written < COL_RESET_SIZE)
-      _reset();
-
-    return count;
+  if (written <= color_code_size) {
+    RESET();
+    return 0;
   }
-  else {
-    struct iovec vec = { (char *)buf, count };
-    return writev(fd, &vec, 1);
+
+  written = ORIGINAL(write)(fd, buf, count);
+  if (written > 0) RESET();
+  return written;
+}
+
+ssize_t FUNC(fwrite)(const void *data, size_t size, size_t count, FILE *stream) {
+  ssize_t result;
+  GET_ORIGINAL(ssize_t, fwrite, const void*, size_t, size_t, FILE *);
+  GET_COLOR_CODE();
+  if (stream == stderr && isatty(STDERR_FILENO))
+    ORIGINAL(fwrite)(color_code, sizeof(char), color_code_size, stream);
+  result = ORIGINAL(fwrite)(data, size, count, stream);
+  if (stream == stderr && isatty(STDERR_FILENO))
+    ORIGINAL(fwrite)(COL_RESET, sizeof(char), COL_RESET_SIZE, stream);
+  return result;
+}
+
+int FUNC(fputc)(int chr, FILE *stream) {
+  const unsigned char c[] = { (unsigned char)chr };
+  return FUNC(fwrite)(c, sizeof(char), sizeof(char), stream);
+}
+
+int _fputs(const char *str, FILE *stream) {
+  return FUNC(fwrite)(str, sizeof(char), strlen(str)/sizeof(char), stream);
+}
+
+int FUNC(vfprintf)(FILE *stream, const char *format, va_list ap) {
+  char *buf = NULL;
+
+  if (vasprintf(&buf, format, ap) > 0) {
+    int result = FUNC(fwrite)(buf, sizeof(char), strlen(buf)/sizeof(char), stream);
+    free(buf);
+    return result;
+  } else {
+    return -1;
   }
 }
+
+int FUNC(fprintf)(FILE *stream, const char *format, ...) {
+  va_list args;
+  va_start(args, format);
+  int result = FUNC(vfprintf)(stream, format, args);
+  va_end(args);
+  return result;
+}
+
+
+static void vperror(const char *format, ...) {
+  va_list ap;
+  va_start(ap, format);
+  FUNC(vfprintf)(stderr, format, ap);
+  va_end(ap);
+}
+
+
+void FUNC(perror)(const char *msg) {
+  if (msg == NULL) {
+    vperror("%s\n", strerror(errno));
+  } else {
+    vperror("%s: %s\n", msg, strerror(errno));
+  }
+}
+
+#ifdef DYLD_INTERPOSE
+  DYLD_INTERPOSE(FUNC(write), write);
+  DYLD_INTERPOSE(FUNC(fputc), fputc);
+  DYLD_INTERPOSE(FUNC(fputs), fputs);
+  DYLD_INTERPOSE(FUNC(fprintf), fprintf);
+  DYLD_INTERPOSE(FUNC(fwrite), fwrite);
+  DYLD_INTERPOSE(FUNC(vfprintf), vfprintf);
+  DYLD_INTERPOSE(FUNC(perror), perror);
+#endif
+
--- test.c
+++ test.c
@@ -1,13 +1,24 @@
 #include <stdio.h>
+#include <unistd.h>
+#include <errno.h>
+#include <stdarg.h>
+
+void test_vfprintf(const char *format, ...) {
+  va_list ap;
+  va_start(ap, format);
+  vfprintf(stderr, format, ap);
+  va_end(ap);
+}
 
 int main() {
   printf("[1] printf\n");
   write(2, "[2] write\n", 11);
   fprintf(stderr, "[2] fprintf\n");
   fwrite("[2] fwrite\n", 12, 1, stderr);
-  fputc(0x32, stderr);
-  printf(" <= fputc\n");
+  fputc(0x32, stderr); printf(" <= fputc\n");
   fputs("[2] fputs\n", stderr);
+  test_vfprintf("[2] %s\n", "vfprintf");
+  errno = ENOSYS; perror("[2] perror");
 
   return 0;
 }
