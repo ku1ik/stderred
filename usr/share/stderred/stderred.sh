335cce20387060265fc7410b86b2db32f3227374
Marcin Kulik <marcin.kulik@gmail.com>
2012-05-13 10:12:03 +0200

    Handle Dynamic String Token ($LIB) on multi-arch distros

--- usr/share/stderred/stderred.sh
+++ usr/share/stderred/stderred.sh
@@ -1,7 +1,9 @@
 # Source this file in your shell to enable stderred for all commands
 
 if [[ ! "$LD_PRELOAD" == *libstderred.so* ]]; then
-  if [[ -f /usr/lib/libstderred.so ]]; then
+  if [[ -f /usr/lib/libstderred.so && -f /usr/lib64/libstderred.so ]]; then
+    export LD_PRELOAD="/usr/\$LIB/libstderred.so${LD_PRELOAD:+:$LD_PRELOAD}"
+  elif [[ -f /usr/lib/libstderred.so ]]; then
     export LD_PRELOAD="/usr/lib/libstderred.so${LD_PRELOAD:+:$LD_PRELOAD}"
   elif [[ -f /usr/lib64/libstderred.so ]]; then
     export LD_PRELOAD="/usr/lib64/libstderred.so${LD_PRELOAD:+:$LD_PRELOAD}"
